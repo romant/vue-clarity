d2a29944ea379e2345ab337a756307ac0e8cf5b6
Roman Tarnavski <roman@romant.net>
2018-03-27 14:17:39 +1100

    clarity init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn">Regular</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-danger">Danger</button>
+    <button class="btn" disabled>Disabled</button>
+
+    <button type="button" class="btn btn-icon">
+    <clr-icon shape="home"></clr-icon>
+    </button>
+    <button type="button" class="btn btn-icon btn-primary">
+        <clr-icon shape="cog"></clr-icon>
+    </button>
+    <button type="button" class="btn btn-icon btn-warning">
+        <clr-icon shape="warning-standard"></clr-icon>
+    </button>
+    <button type="button" class="btn btn-icon btn-danger">
+        <clr-icon shape="error-standard"></clr-icon>
+    </button>
+    <button type="button" class="btn btn-icon btn-success">
+        <clr-icon shape="check"></clr-icon>
+    </button>
+    <button type="button" class="btn btn-icon" disabled>
+        <clr-icon shape="home"></clr-icon>
+    </button>
+
+    <clr-icon shape="info-circle" size="12"></clr-icon>
+    <clr-icon shape="info-circle" size="16"></clr-icon>
+    <clr-icon shape="info-circle" size="36"></clr-icon>
+    <clr-icon shape="info-circle" size="48"></clr-icon>
+    <clr-icon shape="info-circle" size="64"></clr-icon>
+    <clr-icon shape="info-circle" size="72"></clr-icon>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
+import "@clr/ui/clr-ui.css";
+import '@clr/icons';
+import '@clr/icons/shapes/all-shapes';
 
 export default {
   name: 'app',
@@ -17,12 +52,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
